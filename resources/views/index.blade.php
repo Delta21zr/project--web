91804218ffababf83eec0a7adbc545f135ae7fe3
TacoBell-27 <albertogalindo2709@gmail.com>
2025-02-18 12:48:18 -0600

    Descripción breve del cambio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,38 @@
-extends('layouts.master')
+@extends('layouts.master')
 
 @section('content')
 
+<!-- Hero Section -->
 <section id="hero">
     <div class="container">
         <div class="row align-items-center">
-            <!--texto -->
-            <div class="text-start">
+            <!-- Texto alineado a la izquierda -->
+            <div class="col-md-6 text-start">
                 <h1 class="hero-text">EASY TO USE SALE FROM ANY DEVICE. <br> 100% ONLINE.</h1>
                 
                 <h1 class="msj-text">EASY TO USE.</h1>
                 <h1 class="msj-text">SALES FROM ANY DEVICE.</h1>
-                <h1 class="msj-text">100% ONLINE.</h1>
-                    
-                </section>
+                <h1 class="msj-text">100% ONLINE.</h1>
+
+                
                 <button class="btn-custom">START NOW</button>
             </div>
 
-            <!--imagen -->
-    <div class="col-md-6 text-end">
-        <img src="{{ asset('images/fotodemorra.png') }}" alt="Imagen" width="500" height="500" class="img-fluid">
+            <!-- Imagen alineada a la derecha -->
+            <div class="col-md-6 text-end">
+                <img src="{{ asset('images/fotodemorra.png') }}" alt="Imagen" width="500" height="500" class="img-fluid">
             </div>
         </div>
     </div>
 </section>
 
-<section id="Services">
+<!-- Services Section -->
+<section id="Services" class="mt-5">
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -43,48 +45,35 @@ extends('layouts.master')
                 <img src="https://placehold.jp/1200x400.png" class="d-block w-100" alt="...">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 </section>
 
-<section class="section" id="Prices">
+<!-- Prices Section -->
+<section id="Prices" class="section mt-5">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores, dolor dolorum fugiat eligendi sapiente eius praesentium molestias iusto, accusamus quam cupiditate reprehenderit accusantium. Illum aperiam doloremque tempore beatae alias.
-            </div>
-            <div class="col-md-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores, dolor dolorum fugiat eligendi sapiente eius praesentium molestias iusto, accusamus quam cupiditate reprehenderit accusantium. Illum aperiam doloremque tempore beatae alias.
-            </div>
-            <div class="col-md-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maiores, dolor dolorum fugiat eligendi sapiente eius praesentium molestias iusto, accusamus quam cupiditate reprehenderit accusantium. Illum aperiam doloremque tempore beatae alias.
-            </div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet consectetur.</div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet consectetur.</div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet consectetur.</div>
         </div> 
     </div>   
 </section>   
 
-<section id="Choose">
+<!-- Choose Section -->
+<section id="Choose" class="mt-5">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit architecto inventore, beatae corrupti placeat, facere rerum maxime perferendis numquam debitis eveniet impedit culpa laborum. Officiis aspernatur quidem sed deserunt dolores?</div>
-            <div class="col-md-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur corporis possimus sapiente quas, tempore hic. Saepe deleniti nesciunt odit dolores repellat voluptas, natus quia repellendus voluptatem. Sed, ex sequi. Vero.</div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet consectetur.</div>
+            <div class="col-md-8">Lorem ipsum dolor sit amet consectetur.</div>
         </div>
     </div>
 </section>
 
-<section id="Promotion">
+<!-- Promotion Section -->
+<section id="Promotion" class="mt-5">
     <div class="container">
         <div class="row">
-            <div class="col">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas iste ipsam, deserunt eligendi asperiores nemo praesentium quam. Fuga consequatur voluptates possimus blanditiis dicta, officiis aut odio ipsam, rerum, corporis minima?
-            </div>
+            <div class="col">Lorem ipsum dolor sit amet consectetur.</div>
         </div>
     </div>
 </section>
