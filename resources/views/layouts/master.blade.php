74877a818f96ec34bc71736c30322f55ff6f2f7e
TacoBell-27 <albertogalindo2709@gmail.com>
2025-02-21 11:09:03 -0600

    lo desduplique

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,28 +51,6 @@
                         <button class="custom-button">Register</button>
                         <button class="custom-button">Log in</button>
                     </div>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="solutionsDropdown" role="button" data-bs-toggle="dropdown">
-                        SOLUTIONS
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="solutionsDropdown">
-                        <li><a class="dropdown-item" href="#">🖥 Point of Sale</a></li>
-                        <li><a class="dropdown-item" href="#">🛒 Online Store</a></li>
-                        <li><a class="dropdown-item" href="#">📋 Inventory Management</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link active" href="#">COMMERCES</a></li>
-                <li class="nav-item"><a class="nav-link active" href="#">PRICES</a></li>
-
-
-                <!-- Botones de Registro y Log in -->
-                <div class="button-container">
-                    <button class="custom-button">Register</button>
-                    <a href="{{ route('login') }}" class="custom-button">Log in</a>
-                </div>
             </ul>
         </div>
     </div>
