bed68662bcd692cca247223928dec04a1c07cc84
TaTa1414Gus <tata112acdd@gmail.com>
2025-02-26 13:26:58 -0600

    eliminacion de codigo duplicado

--- resources/views/dash/vistas/ventas.blade.php
+++ resources/views/dash/vistas/ventas.blade.php
@@ -202,28 +202,12 @@
                                     </a>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
                 <!-- /.container-fluid -->
-
             </div>
             <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
         <!-- End of Content Wrapper -->
 
     </div>
